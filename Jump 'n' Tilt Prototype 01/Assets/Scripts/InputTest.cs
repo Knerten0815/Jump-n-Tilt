192ee49e1740c200c6ba3c4c0b480f0789423cca
Kevin <kevin_zielke@yahoo.de>
2020-05-13 00:12:39 +0200

    New: InputTest.cs For Testing PlayerInput and learning how to subscribe to events from other classes
    Edit: PlayerInput.cs
    deleted all else's in input-checks and moved Log-calls to InputTest.cs
    
    Edit: Main.unity
    added PlayerInput GameObject for PlayerInput.cs and InputTest.cs
    
    Edit: InputManager.asset
    changed slowMo from X to Y on gamepad

---
+++ Jump 'n' Tilt Prototype 01/Assets/Scripts/InputTest.cs
@@ -0,0 +1,47 @@
+﻿using GameActions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerInput.onMove += Move;
+        PlayerInput.onJump += Jump;
+        PlayerInput.onPlayerAttack += Attack;
+        PlayerInput.onTilt += Tilt;
+        PlayerInput.onSlowMo += SlowMo;
+        PlayerInput.onStomp += Stomp;        
+    }
+
+    private void Move(float horizontal)
+    {
+        Debug.Log("horizontal: " + horizontal);
+    }
+
+    private void Jump()
+    {
+        Debug.Log("jump");
+    }
+    private void Attack()
+    {
+        Debug.Log("attack");
+    }
+
+    private void Tilt(float direction)
+    {
+        Debug.Log("tilt: " + direction);
+    }
+
+    private void SlowMo()
+    {
+        Debug.Log("slowmo");
+    }
+    private void Stomp()
+    {
+        Debug.Log("stomp");
+    }
+}

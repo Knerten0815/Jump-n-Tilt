d793b6b77fe98c85f50252cde4b6c9f6531b7637
NicoleMnrk <nicole.mynarek@gmx.de>
2020-05-13 17:25:17 +0200

    New: Character.class: Class for basic actions for all kind of characters in the game. Subclass of PhysicsObject.class
    protected override void ComputeVelocity():
    Overridden method from PhysicsObject.class. Updates the direction of the sprite according to moving direction.
    
    protected virtual void Movement():
    Basic horizontal movement
    
    protected virtual void Jump():
    Basic jump
    
    void CharacterFacingDirection():
    Method for flipping character sprites according to moving direction
    
    PlayerCharacter.class:
    Class for the player with extended functions.
    
    protected override void ComputeVelocity():
    Movement()- and Jump()-function is called after some input.
    
    protected override void Jump():
    Extended with double jump
    
    What is still missing?
    Sliding down slopes
    go up slopes

---
+++ Jump 'n' Tilt Prototype 01/Assets/Scripts/Character.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : PhysicsObject
+{
+
+    //Author: Nicole Mynarek
+
+    protected float moveDirection;              // Gets value between -1 and 1 for direction left or right if Input.GetAxisRaw is used
+
+    // Values can be adjusted in inspector
+    public float moveSpeed = 5f;                // Movement Speed
+    public float jumpHeight = 16f;
+    public float jumpHeightReducer = 0.5f;      // Reduces jump height, if button is pressed shortly
+
+    private Vector2 slideDirection;
+
+    public bool isFacingRight = true;
+    public bool isSliding;
+
+    protected override void ComputeVelocity()
+    {
+        CharacterFacingDirection();
+        //Slide();
+    }
+
+    // Author: Nicole Mynarek
+    // Method for basic horizontal movement 
+    protected virtual void Movement()
+    {
+        if (grounded)
+        {
+            //Debug.Log("Player kann laufen");
+            velocity = new Vector2(moveDirection * moveSpeed, velocity.y);
+        }
+
+    }
+
+    // Author: Nicole Mynarek
+    // Method for basic jump
+    protected virtual void Jump()
+    {
+        if (jumpable)
+        {
+            velocity = new Vector2(velocity.x, jumpHeight);
+        }
+    }
+
+    // Author: Nicole Mynarek
+    // Method for flipping character sprites according to moving direction
+    void CharacterFacingDirection()
+    {
+        if (isFacingRight && moveDirection < 0)
+        {
+            isFacingRight = !isFacingRight;
+            transform.Rotate(0f, 180f, 0f);
+        }
+        else if (!isFacingRight && moveDirection > 0)
+        {
+            isFacingRight = !isFacingRight;
+            transform.Rotate(0f, 180f, 0f);
+        }
+    }
+
+    // Author: Nicole Mynarek
+    // Method for sliding. Does not work yet.
+    /*protected virtual void Slide()
+    {
+        Vector2 normal;
+
+        if (grounded)
+        {
+            if (groundNormal != new Vector2(0f, 1f))
+            {
+                isSliding = true;
+                Debug.Log("Sliding? " + isSliding);
+
+                for (int i = 0; i < hitBufferList.Count; i++)
+                {
+                    normal = hitBufferList[i].normal;
+                    slideDirection = Vector2.Perpendicular(-normal);
+                    Debug.Log("SlideDirection: " + slideDirection);
+                }
+                //velocity += slideDirection;
+            }
+        }
+    }*/
+}

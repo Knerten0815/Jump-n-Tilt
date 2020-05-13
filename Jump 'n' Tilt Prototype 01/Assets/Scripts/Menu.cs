1f0f20641637617f04637f8acdf107eabf6fcf9b
michi217 <michelle.limbach@gmx.de>
2020-05-13 11:50:24 +0200

    New: Menu class: Interactivity of the menu
    Titlescreen class:
    Interactivity of the titlescreen

---
+++ Jump 'n' Tilt Prototype 01/Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Author: Michelle Limbach
+public class Menu : MonoBehaviour
+{
+    //This method loads a scene (later: a level) by a given name
+    //Used by Button_StartGame
+    public void startGame (string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+
+    //This method closes the whole Application
+    //Used by Button_ExitGame
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}

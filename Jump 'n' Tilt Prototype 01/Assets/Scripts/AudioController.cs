1208ee35e1fa949897cca17300e1cf1ef1918397
Kevin <kevin_zielke@yahoo.de>
2020-06-03 20:51:12 +0200

    New: AudioController.cs added Audio Controller with sounds for tilting, attacking and background Music Ambient003.mp3
    New: Audio.meta
    some audio files

---
+++ Jump 'n' Tilt Prototype 01/Assets/Scripts/AudioController.cs
@@ -0,0 +1,40 @@
+﻿using GameActions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] AudioClip attack;
+    [SerializeField] AudioClip earthquake;
+    [SerializeField] AudioClip music;
+
+    private AudioSource source;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = music;
+        source.Play();
+        PlayerInput.onPlayerAttackDown += playAttack;
+        PlayerInput.onTiltDown += playEarthquake;
+    }
+
+    private void playEarthquake(float direction)
+    {
+        source.PlayOneShot(earthquake);
+    }
+
+    private void OnDisable()
+    {
+        PlayerInput.onPlayerAttackDown -= playAttack;
+    }
+
+    void playAttack()
+    {
+        source.PlayOneShot(attack);
+    }
+}

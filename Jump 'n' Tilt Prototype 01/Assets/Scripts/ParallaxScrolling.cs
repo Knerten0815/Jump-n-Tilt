a8265107a56a68c7c693021e62894d08a3f412ef
Kevin <kevin_zielke@yahoo.de>
2020-07-02 15:40:59 +0200

    Edit: ParallaxScrolling tweaked paralax scrolling script and added commentary
    Edit: Kevins Scenes/Kevins Scene.unity
    added parallaxScrolling to the scene

--- Jump 'n' Tilt Prototype 01/Assets/Scripts/ParallaxScrolling.cs
+++ Jump 'n' Tilt Prototype 01/Assets/Scripts/ParallaxScrolling.cs
@@ -1,26 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
+﻿///Author: Taken from Youtube Channel Dani: https://www.youtube.com/channel/UCIabPXjvT5BVTxRDPCBBOOQ
+///Author: Link to video: https://www.youtube.com/watch?v=zit45k6CUMk
+///Author: Tweaks and summary by Kevin Zielke
+
 using UnityEngine;
+
+/// <summary>
+/// This class controls parallax background scrolling. Backgroundlayers need to be added to the scene.
+/// The backgroundlayers then need to be spread out, so that their width exceeds the doubled camera width and
+/// the center layer lies in the center of the camera.
+/// Each backgroundlayer needs to be assigned to the sorting layer 'Background' and a corresponding layer in its
+/// Sprite Renderer Component.
+/// </summary>
 
 public class ParallaxScrolling : MonoBehaviour
 {
     private float length, startpos;
-    public GameObject cam;
+    //changed name of original variable cam to cameraHolder, to better fit our project
+    private Transform cameraHolder;
     public float parallaxEffect;
 
-    // Start is called before the first frame update
     void Start()
     {
         startpos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
+        //made the cameraHolder private, and assigned the parent, so no drag'n'drop is needed
+        cameraHolder = transform.parent;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
+        float temp = (cameraHolder.position.x * (1 - parallaxEffect));
+        float dist = (cameraHolder.position.x * parallaxEffect);
 
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 

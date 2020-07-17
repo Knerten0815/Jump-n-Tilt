47e80a64fb84b7bbbaa1d9d37634805c5699922a
michi217 <62284237+michi217@users.noreply.github.com>
2020-07-17 09:18:02 +0200

    Main menu and pause menu

---
+++ Jump 'n' Tilt Prototype 01/Assets/Scripts/MenuScripts/AudioManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+//using AudioControlling;
+public class AudioManager : MonoBehaviour
+{
+    public Slider backgroundSlider, soundEffectsSlider;
+    /*private static readonly string FirstPlay = "firstPlay";
+    private static readonly string BackgroundPref = "BackgroundPref";
+    private static readonly string SoundEffectsPref = "SoundEffectsPref";
+    private int firstPlayInt;
+    public Slider backgroundSlider, soundEffectsSlider;
+    private float backgroundFloat, soundEffectsFloat; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
+
+        if(firstPlayInt == 0)
+        {
+            //Default values for first play
+            backgroundFloat = 0.5f;
+            soundEffectsFloat = 0.5f;
+
+            //Set the sliders to the default values
+            backgroundSlider.value = backgroundFloat;
+            soundEffectsSlider.value = soundEffectsFloat;
+
+            //Save the default values for later
+            PlayerPrefs.SetFloat(BackgroundPref, backgroundFloat);
+            PlayerPrefs.SetFloat(SoundEffectsPref, soundEffectsFloat);
+
+            PlayerPrefs.SetInt(FirstPlay, -1);
+        }
+        else
+        {
+            backgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
+            backgroundSlider.value = backgroundFloat;
+
+            soundEffectsFloat = PlayerPrefs.GetFloat(SoundEffectsPref);
+            soundEffectsSlider.value = soundEffectsFloat;
+        }
+    }
+
+    public void SaveSoundSettings()
+    {
+        PlayerPrefs.SetFloat(BackgroundPref, backgroundSlider.value);
+        PlayerPrefs.SetFloat(SoundEffectsPref, soundEffectsSlider.value);
+    }
+
+    private void OnApplicationFocus(bool inFocus)
+    {
+        if (!inFocus)
+        {
+            SaveSoundSettings();
+        }
+    }
+
+    */
+
+}

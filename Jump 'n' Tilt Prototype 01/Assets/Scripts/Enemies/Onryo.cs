d825c565bdb06d6f481bf5c1292f94c43e2ae99a
NicoleMnrk <nicole.mynarek@gmx.de>
2020-07-20 11:21:12 +0200

    Added: Onryo.cs temporary Sprites and player folder Onryo sprite

---
+++ Jump 'n' Tilt Prototype 01/Assets/Scripts/Enemies/Onryo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Onryo : Character
+{
+    public bool movesRight = true;
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        whatIsEnemy = LayerMask.GetMask("Player");
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+    protected override void ComputeVelocity()
+    {
+        base.ComputeVelocity();
+
+        if (movesRight)
+        {
+            Movement(1);
+        }
+        else
+        {
+            Movement(-1);
+        }
+    }
+    protected override void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+}

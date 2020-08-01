8029a130c7eb64232fc43d45627ff4e99c2224e8
katja <katja.tuemmers@gmail.com>
2020-08-01 13:38:39 +0200

    Tested out virtual keyboard for Highscore in TESTINPUT.cs. Management System for menu changes

--- Jump 'n' Tilt Prototype 01/Assets/Scripts/DisplayCollectible.cs
+++ Jump 'n' Tilt Prototype 01/Assets/Scripts/DisplayCollectible.cs
@@ -9,8 +9,12 @@ public class DisplayCollectible : MonoBehaviour
 {
     [SerializeField]
     public CollectibleCard collectible;
+    public int ID;
     public TextMeshProUGUI title;
     public TextMeshProUGUI description;
+    
+  
+
 
     public Image image;
 
@@ -18,9 +22,18 @@ public class DisplayCollectible : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        title.text = "????";
+        description.text = "????";
+        image.sprite = collectible.artwork;
+        ManagementSystem.collectibleOnLoad += overwrite;
+
+
+    }
+
+    public void overwrite(int ID)
+    {
         title.text = collectible.title;
         description.text = collectible.description;
         image.sprite = collectible.artwork;
-
     }
 }

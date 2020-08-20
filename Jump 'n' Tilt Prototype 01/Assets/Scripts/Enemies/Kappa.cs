a664fa376125b09d26d55513c6547dde1ebaa9e4
NicoleMnrk <nicole.mynarek@gmx.de>
2020-08-20 13:18:14 +0200

    added: layer for pickups Enemies are not affected by pickups and arrows Kappa.cs prefab for tengu and FlyerCollider

---
+++ Jump 'n' Tilt Prototype 01/Assets/Scripts/Enemies/Kappa.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Author: Nicole Mynarek
+public class Kappa : GroundEnemy
+{
+
+    //private float speed; 
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        //speed = moveSpeed;
+    }
+
+    protected override void ComputeVelocity()
+    {
+        base.ComputeVelocity();
+
+        //patrol
+        //moveSpeed = speed;
+        if (IsWallAhead(false) == true || isGroundAhead(true) == false)
+        {
+
+            if (isFacingRight == true)
+            {
+                direction = -1;
+                isFacingRight = false;
+            }
+            else
+            {
+                direction = 1;
+                isFacingRight = true;
+            }
+        }
+
+        Movement(direction);
+    }
+}

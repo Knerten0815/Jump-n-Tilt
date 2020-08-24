293a55eef0d86a4fd14ccf95c071371020bb2fca
Melanie <melanie-jaeger-95@web.de>
2020-08-24 11:07:58 +0200

    New: level 1 tutorial edit: object prefabs

---
+++ Jump 'n' Tilt Prototype 01/Assets/Scripts/Objects/Spikes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : Character
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("hit");
+        Attack();
+    }
+}

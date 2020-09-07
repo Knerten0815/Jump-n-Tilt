b911a494cc3b02f31f5aee9b2bce408fa17212db
katja <katja.tuemmers@gmail.com>
2020-09-07 09:02:05 +0200

    New: Highscore Display Scripts
    Modified:
    PlayerCharacter has a SlideDoubleCheck, limit can be set in the inspector

---
+++ Jump 'n' Tilt Prototype 01/Assets/Scripts/MenuScripts/HighScoreScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class HighScoreScene : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI newHighscore;
+    [SerializeField]
+    //private Button newHighscore;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ManagementSystem.displayHighscoreSub += displayScore;
+
+    }
+
+    private void displayScore(string name, int score, int level, int spot)
+    {
+
+    }
+
+    // Update is called once per frame
+    
+}

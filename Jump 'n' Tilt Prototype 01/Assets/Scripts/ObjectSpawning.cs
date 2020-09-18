64ea95516237223dde28a638db6f7a47f10be56b
katja <katja.tuemmers@gmail.com>
2020-09-18 09:38:09 +0200

    Cleaned up fixed minor Issues

--- Jump 'n' Tilt Prototype 01/Assets/Scripts/ObjectSpawning.cs
+++ Jump 'n' Tilt Prototype 01/Assets/Scripts/ObjectSpawning.cs
@@ -7,17 +7,54 @@ public class ObjectSpawning : MonoBehaviour
     // Start is called before the first frame update
     public GameObject[] myObjectsFuture;
     public GameObject[] myObjectsPast;
+    private Vector3 playerPositionPrev;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<PlayerCharacter>() != null)
+        PlayerCharacter player = collision.GetComponent<PlayerCharacter>();
+        if (player != null)
         {
-            foreach (GameObject spike in myObjectsFuture)
+            playerPositionPrev = player.GetComponent<Rigidbody2D>().transform.position;
+            /*foreach (GameObject spike in myObjectsFuture)
             {
                 spike.SetActive(!spike.activeSelf);
             }
             foreach (GameObject spike in myObjectsPast)
             {
                 spike.SetActive(!spike.activeSelf);
+            }*/
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        PlayerCharacter player = collision.GetComponent<PlayerCharacter>();
+        if (player != null)
+        {
+            Vector3 playerPositionCurrently = player.GetComponent<Rigidbody2D>().transform.position;
+            if (playerPositionPrev.x < playerPositionCurrently.x)
+            {
+                playerPositionPrev = player.GetComponent<Rigidbody2D>().transform.position;
+                foreach (GameObject objects in myObjectsFuture)
+                {
+                    objects.SetActive(true);
+                }
+                foreach (GameObject objects in myObjectsPast)
+                {
+                    objects.SetActive(false);
+                }
+
+            }
+            else
+            {
+                foreach (GameObject objects in myObjectsFuture)
+                {
+                    objects.SetActive(false);
+                }
+                foreach (GameObject objects in myObjectsPast)
+                {
+                    objects.SetActive(true);
+                }
+
+
             }
         }
     }

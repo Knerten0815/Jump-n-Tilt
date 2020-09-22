260089384a503dfdcf8d4d64ae370012afb9de68
katja <katja.tuemmers@gmail.com>
2020-09-22 03:39:40 +0200

    Comment

--- Jump 'n' Tilt Prototype 01/Assets/Scripts/HighscorePage.cs
+++ Jump 'n' Tilt Prototype 01/Assets/Scripts/HighscorePage.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Author: Katja Tuemmers
+//When a highscore displaying page in a menu is started, it tells ManagementSystem to update the rankings
 public class HighscorePage : MonoBehaviour
 {
     [SerializeField]
